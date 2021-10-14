9d326f956f597a27b76f1e0f9d982200fc6ba639
Christopher Plummer <chrisplummer0194@gmail.com>
2021-10-13 20:12:57 -0400

    Initial bot with /ping command and test code

---
+++ index.js
@@ -0,0 +1,45 @@
+/*var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Welcome to this page!');
+    res.end();
+}).listen(3000);
+
+const client = require('./client/testClient');
+
+client.hello();*/
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token, clientId, guildId } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+]
+    .map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
+
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(token);
