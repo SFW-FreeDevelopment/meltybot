c313bc6d822b605d716c300864cef244734525d9
Christopher Plummer <chrisplummer0194@gmail.com>
2021-10-14 21:52:05 -0400

    Dummy data and dump to DM

--- index.js
+++ index.js
@@ -3,35 +3,25 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const { token, clientId } = require('./config.json');
 const { Client, Intents } = require('discord.js');
+const { characters } = require('./resourceJson/characters.json');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-const characters = [
-    {
-        name: 'Saber',
-        description: 'You know.'
-    },
-    {
-        name: 'Arcueid',
-        description: 'You know.'
-    }
-];
-
 const commands = [
-    new SlashCommandBuilder().setName('meltybot').setDescription('Replies with test!')
+    new SlashCommandBuilder().setName('meltybot').setDescription('Supplies data for Melty Blood Type Lumina')
         .addSubcommandGroup(subcommandGroup => subcommandGroup
             .setName('character')
             .setDescription('Character data')
             .addSubcommand( subcommand => subcommand
                 .setName('all')
                 .setDescription('All character data')
-            )
-            .addSubcommand( subcommand => subcommand
-                .setName('test')
-                .setDescription('All test character data')
             ))
-        .addSubcommand(subcommandGroup => subcommandGroup
+        .addSubcommandGroup(subcommandGroup => subcommandGroup
             .setName('system')
-            .setDescription('System data')),
+            .setDescription('System data')
+            .addSubcommand( subcommand => subcommand
+                .setName('all')
+                .setDescription('All character data')
+            )),
 ]
     .map(command => command.toJSON());
 
@@ -49,26 +39,48 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
+    const { commandName, options, user } = interaction;
+
+    console.log(JSON.stringify(interaction, (key, value) =>
+        typeof value === 'bigint'
+            ? value.toString()
+            : value));
 
     if (commandName === 'meltybot') {
-        let group = interaction?.options?.data?.find(g => g?.name === 'character');
-        let subCommand = '';
 
-        console.log(interaction);
-        console.log(group);
-        console.log(subCommand?.name);
+        let groupArray = options?.data;
 
-        if (group && subCommand) {
-            let message = await interaction?.user.createDM();
-            await message.send(`You'll eventually get ${subCommand?.name} character data`);
-            //await interaction?.deferReply();
-            await interaction?.reply({content: 'DM sent', ephemeral: true});
-        }
-        else if (interaction?.options.getSubcommand() === 'system') {
-            let message = await interaction?.user.createDM();
-            await message.send(`You'll eventually get system data`);
-            await interaction?.reply({content: 'DM sent', ephemeral: true});
+        console.log(JSON.stringify(groupArray));
+
+        if (groupArray) {
+            if (groupArray.some(g => g.name === 'character')) {
+                const group = groupArray.find(g => g.name === 'character');
+                console.log(group);
+                console.log(group?.options);
+                const message = await user.createDM();
+                const subCommandArray = group?.options
+                if (subCommandArray.some(sc => sc.name === 'all')) {
+                    console.log(characters);
+                    let messageString = '';
+                    for (const character of characters) {
+                        console.log(character)
+                        messageString = messageString.concat(`Id: ${character?.id}\nName: ${character.name}\n\n`);
+                    }
+                    await message.send(messageString);
+                    await interaction?.reply({content: 'DM sent', ephemeral: true});
+                }
+            }
+            else if (groupArray.some(g => g.name === 'system')) {
+                const group = groupArray.find(g => g.name === 'system');
+                const message = await user.createDM();
+                const subCommandArray = group?.options
+                if (subCommandArray.some(sc => sc.name === 'all')) {
+                    await message.send(`Nothing to see here!`);
+                    await interaction?.reply({content: 'DM sent', ephemeral: true});
+                }
+                await message.send(`You'll eventually get system data`);
+                await interaction?.reply({content: 'DM sent', ephemeral: true});
+            }
         }
     }
 });
